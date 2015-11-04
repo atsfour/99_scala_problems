84da7ec26f78ada8677456d580039573fbbe8fc1
atsfour <atsfour@users.noreply.github.com>
2015-11-04 14:35:41 +0900

    solve P-07

--- src/main/scala/com/atsfour/s99/Problem7.scala
+++ src/main/scala/com/atsfour/s99/Problem7.scala
@@ -3,12 +3,22 @@ package com.atsfour.s99
 /**
   * Created by atsfour on 2015/11/04.
   */
-object Problem7 {
+object Problem7 extends Solver {
   val problemNum = 7
   val problemDescriptions = List("""Flatten a nested list structure.""")
   def inputs = List("List(List(1, 1), 2, List(3, List(5, 8)))")
   def results = List(flatten(input).toString)
 
   val input = List(List(1, 1), 2, List(3, List(5, 8)))
-  def flatten(l: List[Any]): List[Any] = ???
+  def flatten(l: List[Any]): List[Any] = {
+    def go(ll: List[Any], acc: List[Any]): List[Any] = ll.headOption match {
+      case None => acc
+      case Some(x) => x match {
+        case Nil => acc
+        case x :: xs => go(ll.tail, Nil) ++ go(xs, x :: acc)
+        case a => go(ll.tail, a :: acc)
+      }
+    }
+    go(l, Nil).reverse
+  }
 }
